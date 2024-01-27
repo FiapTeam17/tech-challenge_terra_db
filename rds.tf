dd8df597c8f6b0c408aa55a36ef0a3ee01bf2f7b
GarciaGP <gabriel.garp@outlook.com>
2024-01-27 17:26:26 -0300

    cria nova instancia de banco na infra

--- rds.tf
+++ rds.tf
@@ -35,5 +35,18 @@ resource "aws_db_instance" "db_instance" {
   instance_class      = "db.t2.micro"
   allocated_storage   = 200
   publicly_accessible = true
-  db_name             = "sgr_database"
+  db_name             = "sgr_database_pedido"
+}
+
+resource "aws_db_instance" "db_instance" {
+  engine              = "mysql"
+  engine_version      = "8.0.31"
+  multi_az            = false
+  identifier          = "sgr-rds-instance"
+  username            = "root"
+  password            = var.mssql_login_pwd
+  instance_class      = "db.t2.micro"
+  allocated_storage   = 200
+  publicly_accessible = true
+  db_name             = "sgr_database_producao"
 }
